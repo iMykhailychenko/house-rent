32f96f3963bf83c7b249fc85ba3adac3f4862e5b
imykhailychenko <igor.c.m@ukr.net>
2021-08-08 17:50:38 +0300

    configurate environment

--- backend/dist/config/app.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    port: +process.env.PORT || 3000,
-    host: process.env.HOST || '0.0.0.0',
-};
-exports.default = config;
-//# sourceMappingURL=app.config.js.map
