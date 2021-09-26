90fd3198f7f64e31a9d017a4696e9925644997d6
imykhailychenko <igor.c.m@ukr.net>
2021-09-26 13:58:25 +0300

    continue work

--- frontend/src/components/pages/posts/single/single.tsx
+++ frontend/src/components/pages/posts/single/single.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement } from 'react';
 
-import { ChildFriendly, People, Pets } from '@material-ui/icons';
-
+import useTrans from '../../../../hooks/trans.hook';
 import { useSinglePostSelector } from '../../../../state/entities/posts/posts.selector';
 import { formatDate } from '../../../../utils/helpers';
 import FullScreenImg from '../../../common/full-screen-img/full-screen-img';
+import ImageWrp from '../../../common/image-wrp/image-wrp';
 import NotFoundPost from '../../../common/not-found/not-found-post/not-found-post';
 import PostCardFooter from '../../../common/post-card/post-card-footer/post-card-footer';
 import Socials from '../../../common/share-links/share-links';
@@ -13,6 +13,7 @@ import Container from '../../../layout/container/container';
 import css from './single.module.scss';
 
 const SinglePostComponent = (): ReactElement => {
+    const trans = useTrans();
     const postState = useSinglePostSelector();
     const postData = postState.data;
 
@@ -41,81 +42,29 @@ const SinglePostComponent = (): ReactElement => {
                                 <p className={css.date}>{formatDate(postData.creationDate)}</p>
                             </div>
                         </div>
-
                         <div className={css.wrap}>
                             <h2 className={css.title}>{postData.title}</h2>
                             <p className={css.text} dangerouslySetInnerHTML={{ __html: postData.description }} />
                         </div>
+                        <PostCardFooter post={postData} />
 
-                        <PostCardFooter size="sm" post={postData} />
+                        <div className={css.row}>
+                            <div className={css.cell}>
+                                <ImageWrp name="bird" />
+                                <p>
+                                    {trans('residentsAmount')}: {postData.residentsAmount}
+                                </p>
+                            </div>
 
-                        <div className={css.wrap}>
-                            <h3 className={css.subtitle}>{'about_author'}</h3>
-                            <table className={css.table}>
-                                <tbody>
-                                    <tr>
-                                        <td className={css.left}>residentsAmount</td>
-                                        <td>
-                                            <div className={css.rowWithIcon}>
-                                                <div className={css.svg}>
-                                                    <People />
-                                                </div>
-                                                <span>{postData.residentsAmount}</span>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className={css.left}>children</td>
-                                        <td>
-                                            <div className={css.rowWithIcon}>
-                                                <div className={css.svg}>
-                                                    <ChildFriendly />
-                                                </div>
-                                                <span>{postData.children || '-'}</span>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td className={css.left}>pets</td>
-                                        <td>
-                                            <div className={css.rowWithIcon}>
-                                                <div className={css.svg}>
-                                                    <Pets />
-                                                </div>
-                                                <span>{postData.pets || '-'}</span>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                            <div className={css.cell}>
+                                <ImageWrp name="coffee" />
+                                <p>{postData.children}</p>
+                            </div>
 
-                        <div className={css.wrap}>
-                            <h3 className={css.subtitle}>{'about_flat'}</h3>
-                            <table className={css.table}>
-                                <tbody>
-                                    <tr>
-                                        <td className={css.left}>houseTypeFilters</td>
-                                        <td>{postData.houseTypeFilters.join(', ')}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={css.left}>roomFilters</td>
-                                        <td>{postData.roomFilters.join(', ')}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={css.left}>priceFilters</td>
-                                        <td>{postData.priceFilters.join(', ')}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={css.left}>cityFilters</td>
-                                        <td>{postData.cityFilters}</td>
-                                    </tr>
-                                    <tr>
-                                        <td className={css.left}>priceFilters</td>
-                                        <td>{postData.districtFilters.join(', ')}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <div className={css.cell}>
+                                <ImageWrp name="coffee" />
+                                <p>{postData.pets}</p>
+                            </div>
                         </div>
                     </>
                 </Container>
