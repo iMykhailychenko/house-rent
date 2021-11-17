a6c7c778b54223cbec06b3cb0a7ae4806d2ea47b
imykhailychenko <igor.c.m@ukr.net>
2021-11-18 00:12:38 +0200

    update dev script

--- dev.sh
+++ dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.dev.yml --env-file ./.env.dev up --build --remove-orphans
+docker-compose -f docker-compose.dev.yml --env-file ./.env.dev up --build --remove-orphans -d
+yarn --cwd frontend dev & yarn --cwd backend start:dev
